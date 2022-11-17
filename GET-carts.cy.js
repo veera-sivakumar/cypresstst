ba9cde18fb2925e6015b44386bb68ac393c7db9f
veera-sivakumar <49038838+veera-sivakumar@users.noreply.github.com>
2022-11-16 19:47:57 -0500

    Update GET-carts.cy.js

--- GET-carts.cy.js
+++ GET-carts.cy.js
@@ -1,15 +1,39 @@
-/// <reference types="cypress" />
+// <reference types=”cypress” />
 
-describe("Given the carts api", () => {
-  context("When I send GET /carrinhos", () => {
-    it("Then it should return a list with all registered products", () => {
-      cy.api({
-        method: "GET",
-        url: "/carrinhos",
-      }).should((response) => {
-        expect(response.status).to.eq(200);
-        expect(response.body.quantidade).to.eq(response.body.carrinhos.length);
-      });
-    });
-  });
+describe(“Open and Search in the site”, () => {
+
+it(“Open URL”, () => {
+
+cy.visit(“https://qaautomationlabs.com/”);
+
 });
+
+it(“Click on Read More “, () => {
+
+cy.get(“.staticslider-button”).click();
+
+});
+
+it(“Verify Particular Blog “, () => {
+
+cy.contains(“How to Setup And Run Cypress Test Cases in Google Cloud?”);
+
+});
+
+it(“Click on Blogs”, () => {
+
+cy.contains(“Blog”).scrollIntoView().click({ force: true });
+
+});
+
+it(“Search the data”, () => {
+
+cy.get(‘[id=”wp-block-search__input-2″]’).scrollIntoView();
+
+cy.get(‘[id=”wp-block-search__input-2″]’).click({ force: true }) .type(“cypress”);
+
+cy.get(‘[id=”search-icon”]’).click({ force: true });
+
+cy.contains(“Search Results for: cypress”);
+
+});});
